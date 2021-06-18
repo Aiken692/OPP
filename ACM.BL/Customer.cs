ea7264f5669071b66fb32b1edfefeda583468726
Walter Ruganzu <rwalter692@gmail.com>
2021-06-18 09:26:37 +0300

    first commit

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int  CustomerId{
+            get;
+            private set;
+        }
+
+        public string EmailAddress { get; set; }
+
+        public string FirstName { get; set; }
+
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+
+
+
+        public Customer()
+        {
+        }
+    }
+}
